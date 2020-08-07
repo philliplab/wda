1a7faf33006ff1910a5531d2497fe486f1d83d4a
Phillip Labuschagne <philliplab@gmail.com>
2020-08-07 08:57:35 -0400

    Add a couple of bug fixes to lead_station

--- load_data.py
+++ load_data.py
@@ -4,12 +4,16 @@ import numpy as np
 import pandas as pd
 import re
 
-def load_station(station, data_dir = 'home/phillipl/0_para/3_resources/noaa_ghcnd/ghcnd_all'):
+def load_station(station, data_dir = '/home/phillipl/0_para/3_resources/noaa_ghcnd/ghcnd_all'):
+    if False: # Debugging notes
+        station = 'USW00026617'
     input_file = data_dir + '/' + station + '.dly'
     input_file = re.sub('//', '/', input_file)
-    col_names = ['station'] + 
+    col_names = ['station'] + \
                 [item for t in [('v_'+str(i+1), 'f_'+str(i+1)) for i in range(31)] 
                       for item in t]
     dat = pd.read_table(input_file, 
-                        sep = '\w+',
+                        sep = '\s+',
+                        header = None,
                         names = col_names)
+    return dat
