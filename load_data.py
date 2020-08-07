4dae12b5f6b7d5e4054ddbaa51387bbe923baade
Phillip Labuschagne <philliplab@gmail.com>
2020-08-07 08:50:07 -0400

    Skeleton of basic data loader

---
+++ load_data.py
@@ -0,0 +1,15 @@
+# Script that contains functions for loading NOAA GHCND data files.
+
+import numpy as np
+import pandas as pd
+import re
+
+def load_station(station, data_dir = 'home/phillipl/0_para/3_resources/noaa_ghcnd/ghcnd_all'):
+    input_file = data_dir + '/' + station + '.dly'
+    input_file = re.sub('//', '/', input_file)
+    col_names = ['station'] + 
+                [item for t in [('v_'+str(i+1), 'f_'+str(i+1)) for i in range(31)] 
+                      for item in t]
+    dat = pd.read_table(input_file, 
+                        sep = '\w+',
+                        names = col_names)
