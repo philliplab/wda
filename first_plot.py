20485c537763d9212a25b4b1e7bc7c8b6cd3aca2
Phillip Labuschagne <philliplab@gmail.com>
2020-08-13 16:54:28 -0400

    Compute and plot devations from all time averages

--- first_plot.py
+++ first_plot.py
@@ -6,14 +6,15 @@ import plotnine as gg
 %matplotlib 
 
 dat = load_data.load_station()
+dat.reset_index(inplace = True)
+dat['decade'] = (dat['year'] // 10) * 10
+
 
 # Plotting the monthly avg TMAX per decade
 
-dat.reset_index(inplace = True)
-
-dat['decade'] = (dat['year'] // 10) * 10
 
 decade_means = dat.query('metric == "TMAX" and decade > 1900 and decade < 2020').groupby(['decade', 'month'])['value'].aggregate(['mean']).reset_index()
+decade_means.columns = ['decade', 'month', 'mean_tmax']
 decade_means['mean_tmax'] = decade_means['mean_tmax']/10
 
 decade_means.columns = ['decade', 'month', 'mean_tmax']
@@ -31,5 +32,30 @@ months_labels = [i.strftime('%b') for i in pd.date_range(start = '2010-01-01',
   + gg.scales.scale_x_continuous(breaks = list(range(1, 13)),
       labels = months_labels)
   + gg.theme(subplots_adjust={'right': 0.75}))
+
+
+# Plot the deviation in the decade average monthly TMAX from the alltime monthly TMAX
+
+overall_means = dat.query('metric == "TMAX" and decade > 1900 and decade < 2020').groupby(['month'])['value'].aggregate(['mean']).reset_index()
+overall_means.columns = ['month', 'mean_tmax']
+overall_means['mean_tmax'] = overall_means['mean_tmax']/10
+
+decade_deviations = decade_means.set_index(['decade', 'month']) - overall_means.set_index(['month'])
+decade_deviations.reset_index(inplace = True)
+
+(ggplot(decade_deviations, aes(x = 'month', y = 'mean_tmax', color = 'decade', group = 'decade'))
+  + geom_line()
+  + gg.labs(x = 'Month',
+            y = 'Deviation of Decade Average\nfrom All-time average',
+            color = 'Decade')
+  + gg.scales.scale_x_continuous(breaks = list(range(1, 13)),
+      labels = months_labels)
+  + gg.theme(subplots_adjust={'right': 0.75}))
+
+
+
+
+
+
 
 
