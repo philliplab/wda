ed1cd931b20de4f82bb4151e3586058f4ee51687
Phillip Labuschagne <philliplab@gmail.com>
2020-08-12 20:31:05 -0400

    YAY we have ggplotnine

--- first_plot.py
+++ first_plot.py
@@ -1,11 +1,30 @@
 import numpy as np
 import pandas as pd
 import load_data
-import matplotlib.pyplot as plt
-plt.show()
+#import matplotlib.pyplot as plt
+#plt.show()
+
+from plotnine import ggplot, geom_point, aes, stat_smooth, facet_wrap, geom_line
+from plotnine.data import mtcars
+
+(ggplot(mtcars, aes('wt', 'mpg', color='factor(gear)'))
+ + geom_point()
+ + stat_smooth(method='lm')
+ + facet_wrap('~gear'))
+
 
 dat = load_data.load_station()
-# %matplotlib inline
+%matplotlib 
+
+# plotting by abusing the column headings
+#pdat = pd.pivot_table(
+#        dat.loc[(slice(None), np.arange(1920, 2021), slice(None), 'TMAX'), 
+#          ('value',)], 
+#  values = 'value', 
+#  index = ['year', 'month'], 
+#  aggfunc = 'mean')
+#
+#pdat.unstack('year').plot()
 
 pdat = pd.pivot_table(
         dat.loc[(slice(None), np.arange(1920, 2021), slice(None), 'TMAX'), 
@@ -14,13 +33,10 @@ pdat = pd.pivot_table(
   index = ['year', 'month'], 
   aggfunc = 'mean')
 
-pdat.unstack('year').plot()
+pdat = pdat.reset_index()
 
-pdat = pd.pivot_table(
-        dat.loc[(slice(None), np.arange(1920, 2021), slice(None), 'TMAX'), 
-          ('value',)], 
-  values = 'value', 
-  index = ['year', 'month'], 
-  aggfunc = 'mean')
-
+(ggplot(pdat, aes(x = 'month', y = 'value', color = 'factor(year)'))
+ + geom_line())
+(ggplot(pdat, aes(x = 'month', y = 'value', color = 'year', group = 'year'))
+ + geom_line())
 #plt.plot(
