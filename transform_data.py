97ca16ac2d9438fd949f0128ff03e6024241123b
Phillip Labuschagne <philliplab@gmail.com>
2020-08-25 17:41:41 -0400

    Refactor and clean-up code that counts missing values

--- transform_data.py
+++ transform_data.py
@@ -5,38 +5,49 @@ def check_station(dat, metric, start_date = None, end_date = None):
     '''
 
     if False:
+        import pandas as pd
+        import numpy as np
         import load_data
         dat = load_data.load_station()
         metric = 'TMAX'
         start_date = None
         end_date = None
-    m_counts = dat.query('metric == "TMAX"').groupby(['station', 'year', 'month'])['value'].aggregate('count')
-    m_counts.reset_index(level = 0, drop = True, inplace = True)
+
+    m_counts = dat. \
+        query('metric == "TMAX"'). \
+        groupby(['station', 'year', 'month']) \
+        ['value']. \
+        aggregate('count')
+    m_counts.reset_index(level = 0, 
+                         drop = True, 
+                         inplace = True)
 
     _, min_year, min_month, min_day, _ = dat.index[0]
     _, max_year, max_month, max_day, _ = dat.index[-1]
 
-    all_days = pd.date_range(start = pd.Timestamp(
-            year = min_year,
-            month = min_month,
-            day = min_day),
-        end = pd.Timestamp(
-            year = max_year,
-            month = max_month,
-            day = max_day),
+    first_day = pd.Timestamp(
+        year = min_year,
+        month = min_month,
+        day = min_day)
+    last_day = pd.Timestamp(
+        year = max_year,
+        month = max_month,
+        day = max_day)
+    all_days_indx = pd.date_range(start = first_day,
+        end = last_day,
         freq = 'D')
-    m_no_missing_counts = pd.Series(np.ones((len(all_days),)),
-            index = all_days)
-    g = m_no_missing_counts.groupby(pd.Grouper(freq='M')).aggregate('count')
+    all_days = pd.Series(np.ones((len(all_days_indx),)),
+            index = all_days_indx)
+    days_pm = all_days. \
+            groupby(pd.Grouper(freq='M')). \
+            aggregate('count')
 
-
-    g2_index = pd.MultiIndex.from_arrays([
-            g.index.get_level_values(0).year,
-            g.index.get_level_values(0).month], 
+    days_pm_indx = pd.MultiIndex.from_arrays([
+            days_pm.index.get_level_values(0).year,
+            days_pm.index.get_level_values(0).month], 
         names = ['year', 'month'])
-    g2 = pd.Series(g.values, 
-        index = g2_index,
+    days_pm = pd.Series(days_pm.values, 
+        index = days_pm_indx,
         name = 'perfect')
-    per_month_missing = g2 - m_counts
-    
+    missing_pm = days_pm - m_counts
 
